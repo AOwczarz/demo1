2985982993317482de6fbfeae26fb6fd6f53cc8d
Adrian Owczarz <adrianowczarz@Adrians-MacBook-Air.local>
2020-03-09 11:36:22 -0700

    push II

---
+++ 2.py
@@ -0,0 +1,23 @@
+import time
+
+time.sleep(1)
+
+print ("Water")
+
+time.sleep(1)
+
+print ("Earth")
+
+time.sleep(1)
+
+print ("Fire")
+
+time.sleep(1)
+
+print ("Air")
+
+time.sleep(1)
+
+print ("CHICKEN STRIPS")
+
+time.sleep(1)
